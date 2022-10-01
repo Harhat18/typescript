fa2c0c9d594c437043aaeee82d5e27931e9a3fd7
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-02 00:41:42 +0300

    add

--- exercise/Productservice.ts
+++ exercise/Productservice.ts
@@ -1,17 +1,37 @@
 import { IProductservice } from "./IProductService";
 import { Product } from "./product";
+import { SimpleDataSource } from "./SimpleDataSource";
 
 class ProductService implements IProductservice {
+  private dataSource: SimpleDataSource;
+  private products: Array<Product>;
+
+  constructor() {
+    this.dataSource = new SimpleDataSource();
+    this.products = Array<Product>();
+    this.dataSource.getProducts().forEach((p) => this.products.push(p));
+  }
+
   getById(id: number): Product {
-    throw new Error("Method not implemented.");
+    return this.products.filter((p) => p.id === id)[0];
   }
   getProducts(): Product[] {
-    throw new Error("Method not implemented.");
+    return this.products;
   }
   saveProduct(product: Product): void {
-    throw new Error("Method not implemented.");
+    if (product.id == 0 || product.id == null) {
+      product.id = this.generateId();
+    }
   }
   deleteproduct(product: Product): void {
     throw new Error("Method not implemented.");
+  }
+  private generateId(): number {
+    let key = 1;
+
+    while (this.getById(key) != null) {
+      key++;
+    }
+    return key;
   }
 }
