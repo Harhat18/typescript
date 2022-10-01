ba356f63b279ef22de19dbac3c1b435823ce8032
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-02 00:19:53 +0300

    add

--- exercise/SimpleDataSource.ts
+++ exercise/SimpleDataSource.ts
@@ -1,13 +1,17 @@
 import { Product } from "./product";
 
 export class SimpleDatasource {
-  private product: Array<Product>;
+  private products: Array<Product>;
+
   constructor() {
-    this.product = new Array<Product>(
+    this.products = new Array<Product>(
       new Product(1, "Samsung S5", "Telefon", 1000),
       new Product(2, "Samsung S6", "Telefon", 2000),
       new Product(3, "Samsung S7", "Telefon", 3000),
       new Product(4, "Samsung S8", "Telefon", 4000)
     );
+  }
+  getProducts(): Product[] {
+    return this.products;
   }
 }
